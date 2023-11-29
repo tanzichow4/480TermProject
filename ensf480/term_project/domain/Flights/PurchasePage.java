75560740fe4a82c4d9d5a3dde5ee3cff20bbde12
Raina <raina.jugdev@ucalgary.ca>
2023-11-28 22:30:11 -0700

    Adding update the seat status - works 100%

--- ensf480/term_project/domain/Flights/PurchasePage.java
+++ ensf480/term_project/domain/Flights/PurchasePage.java
@@ -6,11 +6,13 @@ import ensf480.term_project.domain.Controllers.EmailSender;
 import ensf480.term_project.domain.Users.RegisteredUser;
 import ensf480.term_project.domain.Boundaries.PromoDatabaseHandler;
 import ensf480.term_project.domain.Payments.*;
+import ensf480.term_project.domain.Boundaries.*;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.math.BigDecimal;
+import java.sql.*;
 
 public class PurchasePage extends JFrame {
     private Flight flight;
@@ -106,6 +108,7 @@ public class PurchasePage extends JFrame {
                     // Save the payment to the database
                     if (payment.saveToDatabase()) {
                         // Show a confirmation dialog
+                        updateSeatStatus(selectedSeat);
                         JOptionPane.showMessageDialog(PurchasePage.this, "Purchase confirmed!", "Confirmation",
                                 JOptionPane.INFORMATION_MESSAGE);
 
@@ -117,6 +120,7 @@ public class PurchasePage extends JFrame {
                         EmailSender.sendPurchaseConfirmationEmail(
                             user.getEmail(), flight.getFlightID(), selectedSeat.getSeatId()
                         );
+
                         // Close the PurchasePage window
 
                         // You may also want to close the current PurchasePage or navigate to another
@@ -210,4 +214,33 @@ public class PurchasePage extends JFrame {
         // For simplicity, just check if the fields are not empty
         return !creditCardNumber.trim().isEmpty() && !expiryDate.trim().isEmpty() && !securityCode.trim().isEmpty();
     }
+
+    // Update seat status in the database
+private void updateSeatStatus(Seat selectedSeat) {
+    try {
+        DatabaseManager.connect("AIRLINE");
+        Connection connection = DatabaseManager.getConnection("AIRLINE");
+
+        // Prepare the SQL query for updating the seat's booked status
+        String updateQuery = "UPDATE Seats SET booked = ? WHERE seat_id = ?";
+
+        try (PreparedStatement updateStatement = connection.prepareStatement(updateQuery)) {
+            // Set parameters for the update statement
+            updateStatement.setBoolean(1, true);
+            updateStatement.setInt(2, selectedSeat.getSeatId());
+
+            // Execute the update statement
+            int affectedRows = updateStatement.executeUpdate();
+
+            // Check if the update was successful
+            if (affectedRows > 0) {
+                System.out.println("Seat status updated in the database.");
+            } else {
+                System.err.println("Failed to update seat status in the database.");
+            }
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+}
 }
